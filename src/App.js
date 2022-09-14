f85bf56258ea45ef4016bd15f69577afcc251c30
Emerson Villalta <reaperfocs@gmail.com>
2022-09-14 04:46:33 -0500

    portfolio v1

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          In creation ...
+        </p>
+        <p>
+          For now ...
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/emerson-villalta-1b09a01aa/"
+        >
+          Link de Linkedin
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
